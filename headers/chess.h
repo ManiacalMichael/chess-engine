5b466cdc5ff3e2fc186fdf2ddf499c32ecebaef2
ManiacalMichael <msch414@gmail.com>
2017-04-22 00:18:32 -0700

    rewrote chess.h to new specification

--- headers/chess.h
+++ headers/chess.h
@@ -1,54 +1,12 @@
-/*
- * * * chess.h
- * Representation for the game of chess e.g. boards, pieces, moves, etc.
- */
-
 #ifndef INCLUDE_CHESS_H
 #define INCLUDE_CHESS_H
 
 #include <stdint.h>
-
-
+#include <limits.h>
 
 #define UNIVERSAL_SET 0xffffffffffffffffull
 #define EMPTY_SET 0x0000000000000000ull
-#define FILL_MULTIPLIER 0x0101010101010101ull
-#define MAIN_DIAGONAL 0x8040201008040201ull
-#define MAIN_ANTIDIAGONAL 0x0102040810204080ull
 
-
-
-enum COLORS {
-	WHITE,
-	BLACK
-};
-
-enum PIECETYPES {
-	PAWN = 1,
-	KNIGHT,
-	BISHOP,
-	ROOK,
-	QUEEN,
-	KING
-};
-
-enum PIECES {
-	NO_PIECE,
-	WHITE_PAWN = 2, BLACK_PAWN,
-	WHITE_KNIGHT, BLACK_KNIGHT,
-	WHITE_BISHOP, BLACK_BISHOP,
-	WHITE_ROOK, BLACK_ROOK,
-	WHITE_QUEEN, BLACK_QUEEN,
-	WHITE_KING, BLACK_KING
-};
-
-enum CASTLETYPES {
-	WK_CASTLE, WQ_CASTLE, BK_CASTLE, BQ_CASTLE
-};
-
-/*
- * Little Endian Rank File numbering
- */
 enum SQUARES {
 	S_A1, S_B1, S_C1, S_D1, S_E1, S_F1, S_G1, S_H1,
 	S_A2, S_B2, S_C2, S_D2, S_E2, S_F2, S_G2, S_H2,
@@ -82,57 +40,56 @@ enum FILES {
 	H_FILE
 };
 
-
-/*
- * struct position_t
- * 	pieces: Array of bitboards, index by COLORS and PIECETYPES
- * 	occupied:
- * 	empty: Bitboards, occupied and empty squares of both colors
- * 	captures: Tracks the most recently captured pieces by type,
- * 	          first element is the number of captured pieces
- * 	kingpos: position of kings on board, index by COLORS
- * 	ep_history: record of when and where e.p. squares were set
- * 	            [0][0] = number of e.p. squares recorded
- * 	            [0][x] = square where e.p. was set
- * 	            [1][x] = halfmove when e.p. was set
- * 	castles: record of when castle rights were lost
- * 	flags: Position flags, see #defines for more information
- * 	moves: Age of position, in halfmoves from start position
- * 	fiftymove: Number of halfmoves since an irreversible move took place
- */
-struct position_t {
-	uint64_t pieces[2][7];
-	uint64_t occupied;
-	uint64_t empty;
-	unsigned char captures[31];
-	unsigned char kingpos[2];
-	unsigned short ep_history[2][17];
-	unsigned short castles[4];
-	uint16_t flags;
-	int moves;
-	int fiftymove;
+enum PIECETYPES {
+	PAWN = 1,
+	KNIGHT,
+	BISHOP,
+	ROOK,
+	QUEEN,
+	KING
 };
 
-/*
- * Position flags:
- * 012345	- En Passant square
- * 6		- En Passant available
- * 789a		- Castling status
- * bc		- Check status
- * d		- Game is over
- * e		- Game is drawn
- * f		- White to move
- */
-#define EP_SQUARE 0x003fu
-#define EN_PASSANT 0x0040u
-#define WHITE_KINGSIDE_CASTLE 0x0080u
-#define WHITE_QUEENSIDE_CASTLE 0x0100u
+enum PIECES {
+	WHITE, BLACK,
+	WHITE_PAWN, BLACK_PAWN,
+	WHITE_KNIGHT, BLACK_KNIGHT,
+	WHITE_BISHOP, BLACK_BISHOP,
+	WHITE_ROOK, BLACK_ROOK,
+	WHITE_QUEEN, BLACK_QUEEN,
+	WHITE_KING, BLACK_KING,
+	OCCUPIED, EMPTY
+};
+
+enum CASTLETYPES {
+	WS_CASTLE, WL_CASTLE, BS_CASTLE, BL_CASTLE
+};
+
+#define ISEMPTY(val) (val == INT_MAX)
+
+#define SETEMPTY(val) (val = INT_MAX)
+
+typedef struct {
+	uint64_t bboards[16];
+	int capturestack[30];
+	int capturetop;
+	int epstack[2][16];
+	int eptop;
+	int castles[4];
+	int fiftymovestack[126];
+	int fiftymovetop;
+	uint16_t flags;
+	int halfmove;
+	int fiftymove;
+} position_t;
+
+#define EP_AVAILABLE 0x0001u
+#define EP_SQUARE 0x007eu
+#define WHITE_SHORT_CASTLE 0x0080u
+#define WHITE_LONG_CASTLE 0x0100u
 #define WHITE_BOTH_CASTLE 0x0180u
-#define BLACK_KINGSIDE_CASTLE 0x0200u
-#define BLACK_QUEENSIDE_CASTLE 0x0400u
+#define BLACK_SHORT_CASTLE 0x0200u
+#define BLACK_LONG_CASTLE 0x0400u
 #define BLACK_BOTH_CASTLE 0x0600u
-#define BOTH_KINGSIDE_CASTLE 0x0280u
-#define BOTH_QUEENSIDE_CASTLE 0x0500u
 #define BOTH_BOTH_CASTLE 0x0780u
 #define BLACK_CHECK 0x0800u
 #define WHITE_CHECK 0x1000u
@@ -140,136 +97,129 @@ struct position_t {
 #define GAME_DRAWN 0x4000u
 #define WHITE_TO_MOVE 0x8000u
 
-
-extern const struct position_t START_POSITION;
-
-
-/*
- * Move encoding:
- * 012345	- Start square 
- * 6789ab	- End square 
- * cdef		- Move flags:
- * 0: quiet move
- * 1: double pawn push
- * 2: queenside castle
- * 3: kingside castle
- * 4: capture move
- * 5: e.p. capture
- * 6:
- * 7: 
- * 8: promotion to knight
- * 9: promotion to bishop
- * 10: promotion to rook
- * 11: promotion to queen
- * 12: capture promotion to knight
- * 13: capture promotion to bishop
- * 14: capture promotion to rook
- * 15: capture promotion to queen
- */
-#define START_SQUARE 0x03fu
+#define START_SQUARE 0x003fu
 #define END_SQUARE 0x0fc0u
-#define DOUBLE_PAWN_PUSH 0x1000u
-#define KINGSIDE_CASTLE 0x2000u
-#define QUEENSIDE_CASTLE 0x3000u
-#define CAPTURE_MOVE 0x4000u
-#define EP_CAPTURE 0x5000u
-#define KNIGHT_PROMOTION 0x8000u
-#define BISHOP_PROMOTION 0x9000u
-#define ROOK_PROMOTION 0xa000u
-#define QUEEN_PROMOTION 0xb000u
-#define KNIGHT_CAPTURE_PROMOTION 0xc000u
-#define BISHOP_CAPTURE_PROMOTION 0xd000u
-#define ROOK_CAPTURE_PROMOTION 0xe000u
-#define QUEEN_CAPTURE_PROMOTION 0xf000u
+#define MOVE_FLAGS 0xf000u
+#define PAWN_MOVE 0x1000u
+#define CAPTURE_MOVE 0x3000u
+#define SHORT_CASTLE 0x4000u
+#define DOUBLE_PAWN_PUSH 0x5000u
+#define LONG_CASTLE 0x6000u
+#define PROMO_TO_KNIGHT 0x8000u
+#define PROMO_TO_BISHOP 0x9000u
+#define PROMO_TO_ROOK 0xa000u
+#define PROMO_TO_QUEEN 0xb000u
+#define CAPTURE_PROMO_TO_KNIGHT 0xc000u
+#define CAPTURE_PROMO_TO_BISHOP 0xd000u
+#define CAPTURE_PROMO_TO_ROOK 0xe000u
+#define CAPTURE_PROO_TO_QUEEN 0xf000u
+#define IRREVERSIBLE_MOVE 0x9000u
 
-#define ERROR_MOVE 0xffffu
+typedef struct {
+	int top;
+	uint16_t list[218];
+	int priority[218];
+} movels_t;
 
-/*
- * Maximum number of moves in a legal position (described below) 
- * A list of moves would have size MAX_MOVES + 1
- * (Actually there is no proof that 218 is the max number of moves, but no one
- *  has been able to find a legal position with at least 218 since 1968)
+extern const uint64_t file_masks[8];
+
+extern const uint64_t rank_masks[8];
+
+extern const uint64_t rook_masks[64];
+
+extern const uint64_t bishop_masks[64];
+
+extern const uint64_t rook_magics[64];
+
+extern const uint64_t bishop_magics[64];
+
+extern const uint64_t attack_table[82688];
+
+extern const int rook_index[64];
+
+extern const int bishop_index[64];
+
+extern const uint64_t king_attack[64];
+
+extern const uint64_t knight_attack[64];
+
+extern const uint64_t pawn_doublepush[2][8];
+
+extern const uint64_t pawn_movement[2][64];
+
+extern const uint64_t pawn_attack[2][64];
+
+extern const position_t START_POSITION;
+
+
+/* Returns the number of set bits in a bitboard */
+int popcount(uint64_t);
+
+/* Resets the LS1B of a bitboard and returns the incice of that bit */
+int popfirst(uint64_t*);
+
+/* Returns the LS1B of a bitboard */
+int ls1bindice(uint64_t);
+
+
+/* Pushes a piece onto a position's capture stack */
+void pushcapture(position_t*, enum PIECES);
+
+/* Pops a piece from a position's capture stack */
+enum PIECES popcapture(position_t*);
+
+/* Pushes an square onto a position's e.p. stack */
+void pushep(position_t*, enum SQUARES);
+
+/* Pops a square from a position's e.p. stack */
+enum SQUARES popep(position_t*);
+
+/* Pushes a counter onto a position's fiftymove stack */
+void pushfifty(position_t*, int);
+
+/* Pops a counter from a position's fiftymove stack */
+int popfifty(position_t*);
+
+/* Makes a move on a position */
+void makemove(position_t*, uint16_t);
+
+/* Unmakes a move on a position */
+void unmakemove(position_t*, uint16_t);
+
+/* Returns the attack set of a pawn
+ * Arguments: <enemy pieces>, <empty squares>, <color of pawn>, <pawns position>
  */
-#define MAX_MOVES 218
-/* R6R/3Q4/1Q4Q1/4Q3/2Q4Q/Q4Q2/pp1Q4/kBNN1KB1 w - - 0 1 */
+uint64_t pawn_moves(uint64_t, uint64_t, enum PIECES, enum SQUARES);
 
+/* Returns the attack set of a bishop */
+uint64_t bishop_moves(uint64_t, enum SQUARES);
 
+/* Returns the attack set of a rook */
+uint64_t rook_moves(uint64_t, enum SQUARES);
 
- #ifdef NO_SEARCH_LINKAGE
+/* Returns the attack set of a queen */
+uint64_t queen_moves(uint64_t, enum SQUARES);
 
-/*
- * uint16_t check_status() nop
+/* Returns the check status of a position */
+uint16_t check_status(const position_t);
+
+/* Returns an attack set for long castles */
+uint64_t castle_long(const position_t);
+
+/* Returns an attack set for short castles */
+uint64_t castle_short(const position_t);
+
+/* Serializes moves from an attack set to 16-bit moves and puts them in a movelist
+ * Arguments: <attack set>, <piece to move>, <start square>, <movelist>
  */
-uint16_t check_status(const struct position_t pos) {;}
+void addmoves(uint64_t, enum PIECES, enum SQUARES, movels_t*);
 
- #endif
+/* Generates psuedolegal moves for a position and stores them in a movelist */
+void movegen(position_t, movels_t*);
 
-/*
- * int popcount()
- * Returns the number of set bits in a 64-bit value
- * 	@bb - value to count population of
- */
-int popcount(uint64_t bb);
+/* Initializes attack_table[] */
+void init_attack_table();
 
-/*
- * int ls1bindice()
- * Returns the indice of the ls1b in a 64-bit value
- * 	@bb - value to find ls1b of
- */
-int ls1bindice(uint64_t bb);
-
-/*
- * void push_ep()
- * Adds ep to position history
- * 	@posPtr - pointer to position to add ep to
- * 	@sq - square of ep to add
- */
-void push_ep(struct position_t *posPtr, int sq);
-
-/*
- * int pop_ep()
- * Pops ep from ep history if position age is the most recent ep position
- * 	@posPtr - pointer to position to use
- */
-int pop_ep(struct position_t *posPtr);
-
-/*
- * void push_capture()
- * Adds capture to capture history
- * 	@posPtr - pointer to position to use
- * 	@pt - piecetype of captured piece
- */
-void push_capture(struct position_t *posPtr, int pt);
-
-/*
- * int pop_capture()
- * Pops most recent capture from capture history
- * 	@posPtr - pointer to position to use
- */
-int pop_capture(struct position_t *posPtr);
-
-/*
- * void make_move()
- * Makes a move on a position
- * 	@posPtr - pointer to the position to make the move on
- * 	@mv - move to make
- * Assertions:
- * 	- @mv is non-zero and not equal to ERROR_MOVE
- * 	- (START_SQUARE & @mv) matches a piece of the side to move
- */
-void make_move(struct position_t *posPtr, uint16_t mv);
-
-/*
- * void unmake_move()
- * Unmakes a move on a position
- * NOTE: fiftymove counter cannot be restored after it has been reset to 0
- * 	@posPtr - pointer to the position to unmake the move on
- * 	@mv - move to unmake
- * Assertions:
- * 	- @mv is non-zero and not equal to ERROR_MOVE
- * 	- (START_SQUARE & @mv) matches a piece of the side to 'unmove'
- */
-void unmake_move(struct position_t *posPtr, uint16_t mv);
 
 
 #endif
